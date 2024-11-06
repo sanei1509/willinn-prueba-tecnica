9b3126de6ccf81dad54447da081f6b59a6022445
santiago <santiagoneira52@gmail.com>
2024-11-06 09:54:15 -0300

    Hot fix config of the auth

---
+++ src/app/utils/authConfi.ts
@@ -0,0 +1,35 @@
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // Verificación de que las credenciales existen
+        if (!credentials) return null;
+
+        const { email, password } = credentials;
+
+        // Simulación de autenticación (ejemplo)
+        if (email === "usuarioAdmin@gmail.com" && password === "123456") {
+          return { id: "1", name: "usuario admin", email: "usuarioAdmin@gmail.com" };
+        }
+
+        // Si la autenticación falla
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+};
